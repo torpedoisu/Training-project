8fe9d780532b50b9d36824c9dc17d7a7bedfcd63
Heesom <heesom.hs@gmail.com>
2024-01-26 17:39:34 +0900

    refac: CopyFile
    - 디버깅 로그 추가
    - static에서 인스턴스 메소드로 변경
    - 완료, 예외 처리 로그 수정

--- 1일_파일입출력_스트링/src/fileIO/CopyFile.java
+++ 1일_파일입출력_스트링/src/fileIO/CopyFile.java
@@ -12,38 +12,41 @@ public class CopyFile {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter source file(dir) name: ");
+		System.out.print("Enter source file(dir) name: ");
 		String srcFile = sc.nextLine();
 		
-		System.out.println("Enter destination file(dir) name: ");
+		System.out.print("Enter destination file(dir) name: ");
 		String dstFile = sc.nextLine();;
 		
 		File file = new File(srcFile);
 		File newFile = new File(dstFile);
 
 		try {
+			CopyFile copyFile = new CopyFile();
 			
 			if (file.isFile()) {
 				// 파일이라면 바로 복사
+				System.out.println("Start copying file: " + file.getName() + " ...");
 				Files.copy(file.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);	
 			} else {
 				// 디렉토리라면 중첩되어 있는 파일과 디렉토리 고려해서 복사
-				copyDir(file, newFile);
+				System.out.println("Start copying folder: " + file.getName() + "/ ...");
+				copyFile.copyDir(file, newFile);
 			}
 			
-			System.out.println("=== Success File Copy ===");
+			System.out.println("=== [Success] File Copy ===");
 			
 		} catch (NoSuchFileException e) {
 			e.printStackTrace();
-			System.out.println("=== No Such file === ");
+			System.out.println("=== [Error] No Such file === ");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("=== IOException ===");
+			System.out.println("=== [Error] IOException ===");
 		}
 		
 	}
 	
-	private static void copyDir(File file, File newFile) throws IOException{
+	private void copyDir(File file, File newFile) throws IOException{
 		
 		if (!newFile.exists()) {
 			newFile.mkdir();
