4b2702bddfafccff530729ba7dd7d9071df3df44
Heesom <heesom.hs@gmail.com>
2024-01-26 17:41:40 +0900

    refac: Movefile
    - 디버깅 로그 추가
    - static에서 인스턴스 메소드로 변경
    - 완료, 예외 처리 로그 수정

--- 1일_파일입출력_스트링/src/fileIO/MoveFile.java
+++ 1일_파일입출력_스트링/src/fileIO/MoveFile.java
@@ -13,38 +13,41 @@ public class MoveFile {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter source file(dir) name: ");
+		System.out.print("Enter source file(dir) name: ");
 		String srcFile = sc.nextLine();
 		
-		System.out.println("Enter destination file(dir) name: ");
+		System.out.print("Enter destination file(dir) name: ");
 		String dstFile = sc.nextLine();;
 		
 		File src = new File(srcFile);
 		File dst = new File(dstFile);
 		
 		try {
+			MoveFile moveFile = new MoveFile();
 			
 			if (src.isFile()) {
 				// 파일이라면 바로 이동
+				System.out.println("Start moving file: " + src.getName() + " ...");
 				Files.move(src.toPath(), dst.toPath(), StandardCopyOption.REPLACE_EXISTING);	
 			} else {
-				// 디렉토리라면 중첩되어 있는 파일과 디렉토리 고려해서 이돔 후 원본 삭제
-				moveDir(src, dst);
+				// 디렉토리라면 중첩되어 있는 파일과 디렉토리 고려해서 이동 후 원본 삭제
+				System.out.println("Start moving folder: " + src.getName() + "/ ...");
+				moveFile.moveDir(src, dst);
 			}
 			
-			System.out.println("=== Success File Moving ===");
+			System.out.println("=== [Success] File Moving ===");
 			
 		} catch (NoSuchFileException e) {
 			e.printStackTrace();
-			System.out.println("=== No Such file ===");
+			System.out.println("=== [Error] No Such file ===");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("=== IOException ===");
+			System.out.println("=== [Error] IOException ===");
 		}
 
 	}
 	
-	private static void moveDir(File srcFile, File dstFile) throws IOException{
+	private void moveDir(File srcFile, File dstFile) throws IOException{
 		
 		if (!dstFile.exists()) {
 			dstFile.mkdir();
