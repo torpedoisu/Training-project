a2b3bb4d22171d7b541eff38ed182d31f9b7826f
Heesom <heesom.hs@gmail.com>
2024-01-26 18:13:56 +0900

    refac: ChangeEncoding
    - 디버깅 로그 추가
    - BufferedReader, OuputStreamWriter 객체 선언과 초기화 분리
    - FileReader을 InputStreamReader(new FileInputStream)으로 변경하여 파일
      인코딩 읽어오게 수정
    - finally 추가하여 close() 처리
    - 성공, 예외 처리 로그 수정

--- 1일_파일입출력_스트링/src/string/ChangeEncoding.java
+++ 1일_파일입출력_스트링/src/string/ChangeEncoding.java
@@ -5,38 +5,61 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class ChangeEncoding {
-
-	public static void main(String[] args) throws IOException{
+	
+	
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		OutputStreamWriter osr = null;
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("파일 경로 입력: ");
 		String sourceFilePath = sc.nextLine();
-	
-		FileReader fr = new FileReader(sourceFilePath);
-		BufferedReader br = new BufferedReader(fr);
-		
-		String data; 
-		StringBuilder sb = new StringBuilder();
-		while ((data = br.readLine()) != null) {
-			sb.append(data).append("\n");
+		try {
+			
+			System.out.println("Reading file...");
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFilePath)));
+			
+			String data; 
+			StringBuilder sb = new StringBuilder();
+			while ((data = br.readLine()) != null) {
+				sb.append(data).append("\n");
+			}
+			
+			System.out.println("Encoding file...");
+			osr = new OutputStreamWriter(new FileOutputStream(sourceFilePath), "UTF-8");
+			osr.write(sb.toString());
+			
+			System.out.println("=== [Success] Encoding file ===");
+		} catch (FileNotFoundException e) {
+			System.out.println("=== [Error] Input file not found ===");
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("=== [Error] Unsupported encoding ===");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("=== [Error] IO Exception while encoding ===");
+			e.printStackTrace();
+		} finally {
+			try {
+				System.out.println("Closing stream...");
+				if (br != null) br.close();
+				if (osr != null) osr.close();
+			} catch (IOException e) {
+				System.out.println("=== [Error] IO Exception while closing  ===");
+				e.printStackTrace();
+			}
 		}
-		
-		br.close();
-		
-		FileOutputStream fos= new FileOutputStream(sourceFilePath);
-		OutputStreamWriter osr = new OutputStreamWriter(fos, "UTF-8");
-		osr.write(sb.toString());
-		
-		
-		osr.close();
 		
 	}
 
