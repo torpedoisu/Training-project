4756fd285dda063675f73f317ad303fac315f48d
Heesom <heesom.hs@gmail.com>
2024-01-29 09:21:19 +0900

    refac: ReplaceStringAndStore
    - 시스템 인코딩이 아닌 파일 인코딩 방식 그대로 받아오기 위해
      FileWriter이 아닌 InputStreamReader 사용
    - 문서 인코딩 방식 변경하지 않고 저장하도록 변경

--- 1일_파일입출력_스트링/src/string/ReplaceStringAndStore.java
+++ 1일_파일입출력_스트링/src/string/ReplaceStringAndStore.java
@@ -3,11 +3,14 @@ package string;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -19,26 +22,31 @@ import java.util.stream.Collectors;
 public class ReplaceStringAndStore {
 
 	public static void main(String[] args) {
+		
+		BufferedReader br = null;
+		InputStreamReader isr = null;
+		OutputStreamWriter osw = null;
+		
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("원본 text 파일명:");
+        System.out.print("원본 text 파일명: ");
         String sourceFilePath = sc.nextLine();
 
-        System.out.print("치환될 text 파일명:");
+        System.out.print("치환될 text 파일명: ");
         String targetFilePath = sc.nextLine();
 
-        System.out.print("치환대상 문자열:");
+        System.out.print("치환대상 문자열: ");
         String targetString = sc.nextLine();
 
-        System.out.print("치환할 문자열");
+        System.out.print("치환할 문자열: ");
         String replacementString = sc.nextLine();
 
         try {
-        	 FileReader fr = new FileReader(sourceFilePath);
-        	 BufferedReader br = new BufferedReader(fr);
-        	
-        	 FileWriter pw = new FileWriter(targetFilePath);
+        	 isr = new InputStreamReader(new FileInputStream(sourceFilePath)); 
+        	 br = new BufferedReader(isr);
+        	 osw = new OutputStreamWriter(new FileOutputStream(sourceFilePath), isr.getEncoding());
         	 
+        	 System.out.println("Reading file...");
         	 String data;
         	 StringBuilder sb = new StringBuilder();
         	 while ((data = br.readLine()) != null) {
@@ -46,11 +54,25 @@ public class ReplaceStringAndStore {
         	}
         	
         	String result = sb.toString().replace(targetString, replacementString);
-     		pw.write(result);
-        	pw.close();
+        	
+        	System.out.println("Storing file...");
+     		osw.write(result);
+        	
+     		System.out.println("=== [Success] Replace string and store ===");
         	
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } finally {
+			try {
+				System.out.println("Closing stream...");
+				
+				// Inputstream 관련 - BufferedReader를 마지막으로 감싸줬기에 br만 close
+				if (br != null) br.close();
+				if (osw != null) osw.close();
+			} catch (IOException e) {
+				System.out.println("=== [Error] IO Exception while closing  ===");
+				e.printStackTrace();
+			}
         }
     }
 }
