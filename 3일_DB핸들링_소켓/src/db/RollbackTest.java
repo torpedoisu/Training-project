78b8c7ebf691c32363c553612f1d916cf1a4c853
Heesom <heesom.hs@gmail.com>
2024-01-30 09:27:34 +0900

    test: 롤백 테스트 추가
    - ManageDb 객체 활용해 롤백 후 커밋하여 DB 변화 추적

---
+++ 3일_DB핸들링_소켓/src/db/RollbackTest.java
@@ -0,0 +1,34 @@
+package db;
+
+import java.sql.SQLException;
+
+public class RollbackTest {
+	public static void main (String[] args) {
+		ManageDB db = new ManageDB();
+		try {
+			db.insertEmployee("rollbacktest", "rollback", "rollback");
+			
+			System.out.println("Rollback...");
+			db.jdbcConnection.rollback();
+			
+			db.commit();
+			
+			System.out.println("=== [Success] Testing transaction rollback ===");
+		} catch (SQLException e) {
+			System.out.println("=== [Error] While testing DB ===");
+			e.printStackTrace();
+		} finally {
+			try {
+				db.disconnect();
+			} catch (SQLException e) {
+				System.out.println("=== [Error] While closing connection with DB ===");
+				e.printStackTrace();
+			}
+		}
+	
+		
+		
+		
+		
+	}
+}
